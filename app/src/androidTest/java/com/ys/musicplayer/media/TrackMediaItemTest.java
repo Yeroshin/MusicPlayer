b0097a3ff27578980ca5f0785791a7507415f3a7
Stanislav Yeroshin <twister2004@inbox.ru>
2021-12-15 15:23:44 +0300

    15.12.2021 synchronized access

---
+++ app/src/androidTest/java/com/ys/musicplayer/media/TrackMediaItemTest.java
@@ -0,0 +1,21 @@
+package com.ys.musicplayer.media;
+
+import junit.framework.TestCase;
+
+public class TrackMediaItemTest extends TestCase {
+
+    public void testGetTitle() {
+    }
+
+    public void testOnClick() {
+    }
+
+    public void testSetBackItem() {
+    }
+
+    public void testGetContent() {
+    }
+
+    public void testSetTitle() {
+    }
+}
